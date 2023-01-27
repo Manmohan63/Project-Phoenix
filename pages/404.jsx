ea4f94796af4fb65879f8af785ee152638a21687
Manmohan Mandhan <mandhanmanmohan63@gmail.com>
2023-01-27 23:53:54 +0530

    Made 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Image from 'next/image'
+import styles from '../styles/pnf.module.scss'
+import logo from '../public/logo_center.png'
+import four from '../public/4.png'
+
 
 const Error = () => {
-  return (
-    <div>404</div>
-  )
-}
+    const [counter, setCounter] = useState(5);
 
-export default Error
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setCounter((counter) => counter - 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }, []);
+    
+    return (
+      <div className={styles.container}>
+        <div className={styles.container_inner}>
+          <Image
+            src={four}
+            alt="Phoenix"
+            className={styles.img_four}
+          />
+          <Image
+            src={logo}
+            alt="Phoenix"
+            className={styles.img}
+          />
+          <Image
+            src={four}
+            alt="Phoenix"
+            className={styles.img_four}
+          />
+        </div>
+        <div className={styles.text}>
+          You didn&apos;t break the Internet.but we can&apos;t find the page you&apos;re looking for.
+          We are sorry for the inconvenience. You&apos;ll be redirected to the home page in {counter} seconds.
+        </div>
+      </div>
+    )
+  }
+
+  export default Error
