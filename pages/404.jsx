787036d192afc81bf27968f1a1f23d103cbbb4e6
Manmohan Mandhan <mandhanmanmohan63@gmail.com>
2023-01-28 13:07:41 +0530

    Responsiveness of 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,8 +1,11 @@
+import Head from 'next/head'
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import styles from '../styles/pnf.module.scss'
 import logo from '../public/logo_center.png'
 import four from '../public/4.png'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const Error = () => {
     const [counter, setCounter] = useState(5);
@@ -14,30 +17,42 @@ const Error = () => {
       }
     },[counter]);
     
+    // const router = useRouter();
+    // useEffect(() => {
+    //   if (counter === 0) {
+    //     router.push('/');
+    //   }
+    // });
+
     return (
+      <>
+      <Head>
+        <title>Error | 404</title>
+      </Head>
       <div className={styles.container}>
         <div className={styles.container_inner}>
           <Image
             src={four}
             alt="Phoenix"
-            className={styles.img_four}
+            className={styles["img_four"]}
           />
           <Image
             src={logo}
             alt="Phoenix"
-            className={styles.img}
+              className={styles["img"]}
           />
           <Image
             src={four}
             alt="Phoenix"
-            className={styles.img_four}
+              className={styles["img_four"]}
           />
         </div>
         <div className={styles.text}>
           You didn&apos;t break the Internet.but we can&apos;t find the page you&apos;re looking for.
-          We are sorry for the inconvenience. You&apos;ll be redirected to the home page in {counter} seconds.
+          We are sorry for the inconvenience. You&apos;ll be redirected to the <Link href="/">home</Link> page in {counter} seconds.
         </div>
       </div>
+      </>
     )
   }
 
