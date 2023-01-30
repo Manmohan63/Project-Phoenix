3d551ae2ef81aa2c303271f3468a9b6ad340a58c
Manmohan Mandhan <mandhanmanmohan63@gmail.com>
2023-01-30 20:15:57 +0530

    Worked upon UI

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import styles from '../styles/Navbar.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
+import logo from '../public/logo.png'
+import name from '../public/pic5_zoom.png'
 
 const Navbar = () => {
   return (
     <div className={styles["Navbar"]}>
-      Navbar
+      <Link href='/'>
+        <Image
+          src={logo}
+          alt="Phoenix"
+          className={styles["bird"]}
+        />
+        <Image
+          src={name}
+          alt="logo"
+          className={styles["name"]}
+        />
+      </Link>
+      <div className={styles["options"]}>
+        <Link href='/'>Sign up </Link> |
+        <Link href='/'> Sign in</Link>
+      </div>
     </div>
   )
 }
