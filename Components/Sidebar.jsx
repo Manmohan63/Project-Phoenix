29cb707cfba5b249408f33302881f6c18ff89dfb
Manmohan Mandhan <mandhanmanmohan63@gmail.com>
2023-02-01 03:22:50 +0530

    Fixed Icons and Other designs, struggling with page gradient

--- Components/Sidebar.jsx
+++ Components/Sidebar.jsx
@@ -13,64 +13,76 @@ const Sidebar = () => {
   return (
     <div className={styles["sidebar"]}>
       <div className={styles["hello"]}>
-        <Link href="/">
-          <Image
-            src={home}
-            alt="home"
-            className={styles["images"]}
-          />
-          <div>Home</div>
-        </Link>
+        <div>
+          <Link href="/">
+            <Image
+              src={home}
+              alt="home"
+              className={styles["images"]}
+            />
+          </Link>
+        </div>
+        <div className={styles['textcontainer']}>Home</div>
       </div>
       <div className={styles["hello"]}>
-        <Link href="/chatroom">
-          <Image
-            src={chatroom}
-            alt="chatroom"
-            className={styles["images"]}
-          />
-          <div>Chatroom</div>
-        </Link>
+        <div className={styles["imagecontainer"]}>
+          <Link href="/chatroom">
+            <Image
+              src={chatroom}
+              alt="chatroom"
+              className={styles["images"]}
+            />
+          </Link>
+        </div>
+        <div className={styles['textcontainer']}>Chatroom</div>
       </div>
       <div className={styles["hello"]}>
-        <Link href="/calender">
-          <Image
-            src={calender}
-            alt="calender"
-            className={styles["images"]}
-          />
-          <div>Calender</div>
-        </Link>
+        <div className={styles["imagecontainer"]}>
+          <Link href="/calender">
+            <Image
+              src={calender}
+              alt="calender"
+              className={styles["images"]}
+            />
+          </Link>
+        </div>
+        <div className={styles['textcontainer']}>Calender</div>
       </div>
       <div className={styles["hello"]}>
-        <Link href="/Resources">
-          <Image
-            src={resources}
-            alt="Resources"
-            className={styles["images"]}
-          />
-          <div>Resources</div>
-        </Link>
+        <div className={styles["imagecontainer"]}>
+          <Link href="/resources">
+            <Image
+              src={resources}
+              alt="Resources"
+              className={styles["images"]}
+            />
+          </Link>
+        </div>
+        <div className={styles['textcontainer']}>Resources</div>
       </div>
       <div className={styles["hello"]}>
-        <Link href="/more">
-          <Image
-            src={more}
-            alt="more"
-            className={styles["images"]}
-          />
-          <div>More</div>
-        </Link>
+        <div className={styles["imagecontainer"]}>
+          <Link href="/more">
+            <Image
+              src={more}
+              alt="more"
+              className={styles["images"]}
+            />
+          </Link>
+        </div>
+        <div className={styles['textcontainer']}>More</div>
       </div>
       <div className={styles["hello"]}>
-        <Link href="/settings">
-          <Image
-            src={settings}
-            alt="settings"
-            className={styles["images"]}
-          />
-          <div>settings</div>
-        </Link>
+        <div className={styles["imagecontainer"]}>
+          <Link href="/settings">
+            <Image
+              src={settings}
+              alt="settings"
+              className={styles["images"]}
+            />
+          </Link>
+        </div>
+        <div className={styles['textcontainer']}>settings</div>
       </div>
     </div>
   )
