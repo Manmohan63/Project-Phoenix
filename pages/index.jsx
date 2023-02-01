7f963ec12f092878486c1aef063c350fb79362fa
ishikapoddar <ishikapoddar30@gmail.com>
2023-02-01 14:02:29 +0530

    up

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+// import React from 'react'
 
-const index = () => {
-  return (
-    <div>index hello hello</div>
-  )
-}
+// const index = () => {
+//   return (
+//     <div>index hello hello</div>
+//   )
+// }
 
-export default index
+// export default index
+// import React, { useState, useEffect } from 'react';
+
+// const Codeforces = () => {
+//   const [data, setData] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchData() {
+//       const res = await fetch('https://codeforces.com/api/user.info?handles=ninjamayank');
+//       const result = await res.json();
+//       setData(result.result);
+//     }
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div>
+//       {data.map(item => (
+//         <div key={item.handle}>
+//           <p>{item.handle}</p>
+//           <p>{item.rank}</p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Codeforces;
