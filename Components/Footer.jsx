af85b8ca6a0e5b4e075b19d8e3a58cb8671e9719
Manmohan Mandhan <mandhanmanmohan63@gmail.com>
2023-03-10 00:34:25 +0530

    Changes rest scss file to tailwind

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styles from '../styles/Footer.module.scss'
 import { SiYoutube } from 'react-icons/si'
 import { BsInstagram } from 'react-icons/bs'
 import { GrMail } from 'react-icons/gr'
@@ -8,20 +7,19 @@ import Link from 'next/link';
 const Footer = () => {
 
   return (
-    <div className={styles["Footer"]}>
-      <div className={styles["container"]}>
-
-        <div className={styles["icons"]}>
-          <a href="mailto:cpunofficial101@gmail.com" className="text-4xl mx-3" target={"blank"} title={"cpunofficial101@gmail.com"}><GrMail /></a>
-          <a href="https://www.instagram.com/cp_unofficial.101/" className={"text-4xl mx-3"} target={"blank"} title={"Instagram"}><BsInstagram /></a>
-          <a href="https://www.youtube.com/@cpunofficial1196/videos" className={"text-4xl mx-3"} target={"blank"} title={"Youtube"}><SiYoutube /></a>
+    <div className={"flex justify-center items-center relative bottom-0 right-0 border-t-2 border-x-2 border-main bg-cover bg-center backdrop-blur-sm bg-fixed"}>
+      <div className={"flex flex-col justify-center items-center"}>
+        <div className={"flex justify-around items-center py-[0.4rem] px-[10rem] text-main w-[45%] text-2xl"}>
+          <a href="mailto:cpunofficial101@gmail.com" className={"text-4xl mx-3 hover:scale-110  transition-all duration-300 "} target={"blank"} title={"cpunofficial101@gmail.com"}><GrMail /></a>
+          <a href="https://www.instagram.com/cp_unofficial.101/" className={"text-4xl mx-3 hover:scale-110  transition-all duration-300 "} target={"blank"} title={"Instagram"}><BsInstagram /></a>
+          <a href="https://www.youtube.com/@cpunofficial1196/videos" className={"text-4xl mx-3 hover:scale-110  transition-all duration-300 "} target={"blank"} title={"Youtube"}><SiYoutube /></a>
         </div>
-        <div className={styles["text"]}>
+        <div>
           <Link href='/about' title={"About Us"}>About Us</Link>&nbsp; &nbsp;
           <Link href='/contact' title={"Contact Us"}>Contact us</Link>&nbsp; &nbsp;
           <a href='https://github.com/Manmohan63/project-phoenix' title={"Github"} target={"blank"}>Github</a>
         </div>
-        <div className={styles["copyright"]}>
+        <div>
           <p>© {new Date().getFullYear()} <a href="https://www.linkedin.com/in/mandhan63/" target={"blank"} title={"Manmohan's LinkedIn"}>Manmohan</a> and <a href="https://www.linkedin.com/in/ishika-poddar30/" target={"blank"} title={"Ishika's LinkedIn"}>Ishika</a>, India</p>
         </div>
 
--- Components/Sidebar.jsx
+++ Components/Sidebar.jsx
@@ -1,87 +1,92 @@
 import React from 'react'
 import Link from 'next/link';
-import styles from '../styles/Sidebar.module.scss'
 import Image from 'next/image';
-import home from '../public/Sidebar Icons/Home.png'
-import chatroom from '../public/Sidebar Icons/Chatroom.png'
-import calender from '../public/Sidebar Icons/calender.png'
-import resources from '../public/Sidebar Icons/resources.png'
-import more from '../public/Sidebar Icons/more.png'
-import settings from '../public/Sidebar Icons/settings.png'
 
 const Sidebar = () => {
   return (
-    <div className={styles["sidebar"]}>
+    <div className={"fixed flex flex-col justify-evenly items-center bg-gradient-to-t from-purple-900 to-purple-800 text-yellow-400 border-r border-yellow-400 top-0 left-0 bottom-0 pt-[4vw] w-[8vw] text-main"}>
       <Link href="/">
-        <div className={styles["hello"]} title={"Home"}>
+        <div className={"border-2 border-solid border-main flex justify-center items-center h-[5vw] w-[5vw] rounded-md flex-col p-[5px] transition-all duration-500 ease-in-out hover:box-shadow "} title={"Home"}>
           <div>
             <Image
-              src={home}
+              src={"https://res.cloudinary.com/dk8ign4oc/image/upload/v1678388162/Home_h0wfwc.png"}
               alt="home"
-              className={styles["images"]}
+              className={"w-[3.5vw] h-auto"}
+               height={128}
+              width={128}
             />
           </div>
-          <div className={styles['textcontainer']}>Home</div>
+          <div className={"flex justify-center items-center"}>Home</div>
         </div>
       </Link>
       <Link href="/chatroom">
-        <div className={styles["hello"]} title={"Chatroom"}>
-          <div className={styles["imagecontainer"]}>
+        <div className={"border-2 border-solid border-main flex justify-center items-center h-[5vw] w-[5vw] rounded-md flex-col p-[5px] transition-all duration-500 ease-in-out hover:box-shadow "} title={"Chatroom"}>
+          <div>
             <Image
-              src={chatroom}
+              src={"https://res.cloudinary.com/dk8ign4oc/image/upload/v1678388161/Chatroom_cfiypu.png"}
               alt="chatroom"
-              className={styles["images"]}
+              className={"w-[3.5vw] h-auto"}
+               height={128}
+              width={128}
             />
           </div>
-          <div className={styles['textcontainer']}>Chatroom</div>
+          <div className={"flex justify-center items-center"}>Chatroom</div>
         </div>
       </Link>
       <Link href="/calender">
-        <div className={styles["hello"]} title={"Calender"}>
-          <div className={styles["imagecontainer"]}>
+        <div className={"border-2 border-solid border-main flex justify-center items-center h-[5vw] w-[5vw] rounded-md flex-col p-[5px] transition-all duration-500 ease-in-out hover:box-shadow "} title={"Calender"}>
+          <div>
             <Image
-              src={calender}
+              src={"https://res.cloudinary.com/dk8ign4oc/image/upload/v1678388162/calender_muwd3r.png"}
               alt="calender"
-              className={styles["images"]}
+              className={"w-[3.5vw] h-auto"}
+               height={128}
+              width={128}
             />
           </div>
-          <div className={styles['textcontainer']}>Calender</div>
+          <div className={"flex justify-center items-center"}>Calender</div>
         </div>
       </Link>
       <Link href="/resources">
-        <div className={styles["hello"]}>
-          <div className={styles["imagecontainer"]} title={"Resources"}>
+        <div className={"border-2 border-solid border-main flex justify-center items-center h-[5vw] w-[5vw] rounded-md flex-col p-[5px] transition-all duration-500 ease-in-out hover:box-shadow "}>
+          <div title={"Resources"}>
             <Image
-              src={resources}
+              src={"https://res.cloudinary.com/dk8ign4oc/image/upload/v1678388162/resources_noksuu.png"}
               alt="Resources"
-              className={styles["images"]}
+              className={"w-[3.5vw] h-auto"}
+               height={128}
+              width={128}
             />
           </div>
-          <div className={styles['textcontainer']}>Resources</div>
+          <div className={"flex justify-center items-center"}>Resources</div>
         </div>
       </Link>
       <Link href="/more">
-        <div className={styles["hello"]} title={"More"}>
-          <div className={styles["imagecontainer"]}>
+        <div className={"border-2 border-solid border-main flex justify-center items-center h-[5vw] w-[5vw] rounded-md flex-col p-[5px] transition-all duration-500 ease-in-out hover:box-shadow "} title={"More"}>
+          <div>
             <Image
-              src={more}
+              src={"https://res.cloudinary.com/dk8ign4oc/image/upload/v1678388162/more_pmz8zy.png"}
               alt="more"
-              className={styles["images"]}
+              className={"w-[3.5vw] h-auto "}
+               height={128}
+              width={128}
             />
           </div>
-          <div className={styles['textcontainer']}>More</div>
+          <div className={"flex justify-center items-center"}>More</div>
         </div>
       </Link>
       <Link href="/settings">
-        <div className={styles["hello"]} title={"Settings"}>
-          <div className={styles["imagecontainer"]}>
+        <div className={"border-2 border-solid border-main flex justify-center items-center h-[5vw] w-[5vw] rounded-md flex-col p-[5px] transition-all duration-500 ease-in-out hover:box-shadow "} title={"Settings"}>
+          <div>
             <Image
-              src={settings}
+              src={"https://res.cloudinary.com/dk8ign4oc/image/upload/v1678388162/settings_gr7zcq.png"}
               alt="settings"
-              className={styles["images"]}
+              className={"w-[3.5vw] h-auto"}
+               height={128}
+              width={128}
             />
           </div>
-          <div className={styles['textcontainer']}>settings</div>
+          <div className={"flex justify-center items-center"}>settings</div>
         </div>
       </Link>
     </div>
