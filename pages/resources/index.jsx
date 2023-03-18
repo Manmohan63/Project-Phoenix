650f6566413759d4a5ae37c5c445bf815fc26aab
Manmohan Mandhan <mandhanmanmohan63@gmail.com>
2023-03-19 01:10:19 +0530

    Setting Up Marquee and Framer Motion

--- pages/resources/index.jsx
+++ pages/resources/index.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
-import { useState } from 'react';
-import Tools from '../tools';
-import Browser_extensions from '../browser_extensions';
+import Marquee from "react-fast-marquee";
+import Marquee_content from './Marquee_content';
 
-const Resource = () => {
-  const [open, setopen] = useState(true);
+const index = () => {
   return (
-    <div className=''>
-      <div className='w-full flex items-center border-b-2'>
-        <div className={`border-x-[1px] m-0 p-0 w-1/2 flex items-center justify-center backdrop-blur-sm ${open? ("text-dark__blue bg-main"): "m-0"}`} onClick={()=>setopen(true)}>Tools</div>
-        <div className={`border-x-[1px] m-0 p-0 w-1/2 flex items-center justify-center backdrop-blur-sm ${!open ? ("text-dark__blue bg-main"): "m-0"}`} onClick={()=>setopen(false)}>Broswser Extensions</div>
-      </div>
-      <div className=''>
-        {open && <Tools />}
-        {!open && <Browser_extensions />}
-      </div>
-    </div>
+    <Marquee
+      pauseOnHover={true}
+      pauseOnClick={true}
+      loop={0}
+      speed={15} 
+      gradient={false}
+    >
+      <Marquee_content />
+    </Marquee>
   )
 }
 
-export default Resource
+export default index
