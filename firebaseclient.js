d37e09a748bc77d05dde56049483d4fee3fa2914
ishikapoddar <ishikapoddar30@gmail.com>
2023-03-19 11:24:41 +0530

    Added Basic Layout of Profile Page

---
+++ firebaseclient.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  // Your Firebase project configuration
+  // You can find this in your Firebase console
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
---
+++ pages/profile/index.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import { getFirestore, doc, getDoc } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import Link from 'next/link';
+import firebaseClient  from '../../firebaseclient';
+
+const Profile = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const auth = getAuth(firebaseClient);
+
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const db = getFirestore(firebaseClient);
+        const userDoc = await getDoc(doc(db, 'users', user.uid));
+        setUser(userDoc.data());
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
+  if (!user) {
+    return (
+      <div>
+        <p>Please sign up to view your profile.</p>
+        {/* <Link href="/signup"><a>Sign up</a></Link> */}
+      </div>
+    );
+  }
+  
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <p>Date of Birth: {user.dateOfBirth}</p>
+      <p>City: {user.city}</p>
+      <p>Codeforces ID: {user.codeforcesId}</p>
+     
+    </div>
+  );
+};
+
+export default Profile;
